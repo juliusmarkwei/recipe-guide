eb6ecfab6650784ff178885688da8a05a4fee420
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-05-27 04:47:09 +0000

    feat: auth service

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: use_key_in_widget_constructors
 class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -8,6 +9,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _loginFormKey = GlobalKey();
+  String? _username;
+  String? _password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,17 +53,26 @@ class _LoginPageState extends State<LoginPage> {
       width: MediaQuery.of(context).size.width * 0.90,
       height: MediaQuery.of(context).size.height * 0.30,
       child: Form(
+        key: _loginFormKey,
         child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             TextFormField(
+              initialValue: 'emilys',
+              onSaved: (newValue) => setState(() => _username = newValue),
+              validator: (value) => value!.isEmpty ? 'Please enter username' : null,
               decoration: const InputDecoration(
                 hintText: 'Username',
               ),
             ),
             TextFormField(
+              initialValue: 'emilyspass',
+              onSaved: (newValue) => setState(() => _password = newValue),
+              obscureText: true,
+              validator: (value) =>
+                  value!.isEmpty || value.length < 5 ? 'Please enter a valid password' : null,
               decoration: const InputDecoration(
                 hintText: 'Password',
               ),
@@ -74,7 +88,12 @@ class _LoginPageState extends State<LoginPage> {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.90,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          if (_loginFormKey.currentState?.validate() ?? false) {
+            _loginFormKey.currentState?.save();
+            debugPrint('Username: $_username, Password: $_password');
+          }
+        },
         child: const Text('Login'),
       ),
     );
