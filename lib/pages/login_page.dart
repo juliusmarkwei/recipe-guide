9a4420bbbcdce4a86d5d4672a8e6150b66abd556
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-05-27 03:57:29 +0000

    feat: login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,8 +10,73 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: SafeArea(child: _buildUI()),
+    );
+  }
+
+  Widget _buildUI() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _title(),
+          _loginForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget _title() {
+    return const Text(
+      'Recipe Book',
+      style: TextStyle(
+        fontSize: 35,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.90,
+      height: MediaQuery.of(context).size.height * 0.30,
+      child: Form(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Username',
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.90,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Login'),
+      ),
     );
   }
 }
