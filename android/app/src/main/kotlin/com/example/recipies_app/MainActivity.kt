073e545ba867863f915a131d4ec2ad728c8926f7
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-05-27 03:27:42 +0000

    project startup files

---
+++ android/app/src/main/kotlin/com/example/recipies_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.recipies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:recipies_app/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Book App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
